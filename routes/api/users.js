2687c20e99303a54d44b07ca0371ce0eff8dfa6a
shazy89 <erdo.shazy123@gmail.com>
2021-01-17 09:30:06 -0500

    jwt, secret key

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult} = require('express-validator');
 
 const User = require('../../models/User');
@@ -16,17 +18,19 @@ router.post('/',[
       check('password', 'Please enter a password with 6 or more characters').isLength({min: 6})
 ], async (req, res) => {
     const errors = validationResult(req);
+
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array()})
-    }
+    };
 
     const { name, email, password } = req.body;
-    
+
     try {
         let user = await User.findOne({ email });
           if(user) {
-            return res.status(400).json({errors: [{msg: 'User alredy exist'}]});
-          }
+            return res.status(400).json({errors: [{msg: 'User alredy exist'}]
+        });
+     };
         const avatar = gravatar.url(email, { 
             s: '200',
             r: 'pg',
@@ -42,7 +46,12 @@ router.post('/',[
 
         await user.save();
 
-        res.send('User registered')
+        const payload = {
+           user: {
+               id: user.id
+           }
+        };
+        jwt.sign(payload, )
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error')
