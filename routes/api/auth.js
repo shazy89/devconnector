0415c3b7fd920f17e9a293cf59e675ec38355a06
shazy89 <erdo.shazy123@gmail.com>
2021-01-17 09:59:24 -0500

    working user auth

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../midlaware/auth')
+const auth = require('../../midlaware/auth');
+const User = require('../../models/User')
 // @route  GET api/auth
 // @desc   Test route
 // @access Public
-router.get('/', auth, (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+      const user = await User.findById(req.user.id).select('-password') // -password pretend the password from being sent
+      res.json(user);
+  } catch (error) {
+      console.error(err.message)
+      res.status(500).send('Server Error');
+  };
+});
 
 module.exports = router;
