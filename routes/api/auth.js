efbf11aedc7d67171eee99c67832be6208046e8f
shazy89 <erdo.shazy123@gmail.com>
2021-01-21 15:48:23 -0500

    add check objectId to middleware

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const auth = require('../../midlaware/auth');
+const auth = require('../../middleware/auth');
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const config = require('config');
 const router = express.Router();
-const auth = require('../../midlaware/auth');
+const auth = require('../../middleware/auth');
 const { check, validationResult} = require('express-validator');
 
 
