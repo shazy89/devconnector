ec3138be7d90a234c7fe743be7947db8c2736d87
shazy89 <erdo.shazy123@gmail.com>
2021-01-21 15:10:35 -0500

    GET api/posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -46,11 +46,26 @@ router.post(
 
   router.get('/', auth, async (req, res) => {
       try {
-          
+          const posts = await Post.find().sort({ date: -1 }); // -1 is for most rcent post for oldest we can do just 1
+          res.json(posts)
       } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
       }
   })
+
+    // @route    GET api/posts/:id
+  // @desc     Get post by Id
+  // @access   Private
+
+  router.get('/', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        res.json(post)
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
