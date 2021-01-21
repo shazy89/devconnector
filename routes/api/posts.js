85c8a57520bdce5dee08b797e8014a8672bbe370
shazy89 <erdo.shazy123@gmail.com>
2021-01-21 14:49:21 -0500

    finish POST api/posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,39 +1,43 @@
+
 const express = require('express');
 const router = express.Router();
-const { check, validationResult} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../midlaware/auth');
 
 const Post = require('../../models/Post');
-const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
-// @route  POST api/posts
-// @desc   Create a post
-// @access Private 
-router.post('/', [auth, 
-    check('text', 'Text is required').not().isEmpty()
-], async (req, res) => {
-
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        res.status(400).json({ errors: errors.array() })
-    };
-    try {
+// @route    POST api/posts
+// @desc     Create a post
+// @access   Private
+router.post(
+    '/',
+    auth,
+    check('text', 'Text is required').not().isEmpty(),
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      try {
         const user = await User.findById(req.user.id).select('-password');
-
-        const newPost = {
-            text: req.body.text,
-            name: user.name,
-            avatar: user.avatar,
-            user: req.user.id
-        }
-        
-    } catch (err) {
+  
+        const newPost = new Post({
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+          user: req.user.id
+        });
+  
+        const post = await newPost.save();
+  
+        res.json(post);
+      } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
+      }
     }
-
-
-});
+  );
 
 module.exports = router;
