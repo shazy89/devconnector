3c9b655091ff809c75c34ca3243ad3ac0ce15984
shazy89 <erdo.shazy123@gmail.com>
2021-01-21 14:00:19 -0500

    last changes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const fetch = require('node-fetch');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../midlaware/auth');
 const { check, validationResult} = require('express-validator');
+
+
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -248,5 +252,25 @@ router.put('/education', [auth,
        }
    })
 
+// @route    GET api/profile/github/:username
+// @desc     Get user repos from Github
+// @access   Public
+router.get('/github/:username', async (req, res) => {
+    try {
+      const uri = encodeURI(
+        `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc`
+      );
+      const headers = {
+        'user-agent': 'node.js',
+        Authorization: `token ${config.get('githubToken')}`
+      };
+  
+      const gitHubResponse = await axios.get(uri, { headers });
+      return res.json(gitHubResponse.data);
+    } catch (err) {
+      console.error(err.message);
+      return res.status(404).json({ msg: 'No Github profile found' });
+    }
+  });
 module.exports = router;
         
