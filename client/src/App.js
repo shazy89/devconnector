a54b4a3e26e63cf209e76653a68c1291d985dc48
shazy89 <erdo.shazy123@gmail.com>
2021-01-24 16:40:18 -0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,19 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
 import './App.css';
 
 const App = () => {
   return (
-    <Fragment>
-       <Navbar />
-       <Landing />
-    </Fragment>
+    <Router>
+        <Fragment>
+           <Navbar />
+           <Switch>
+            <Route exact path='/' component={Landing} />
+           </Switch>
+        </Fragment>
+    </Router>
   );
 }
 
