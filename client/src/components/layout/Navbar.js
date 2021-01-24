64e912019e12e942f68882cf50bead0010dae71a
shazy89 <erdo.shazy123@gmail.com>
2021-01-24 16:58:34 -0500

    register form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
           <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
         </h1>
         <ul>
-          <li><Link to="profiles.html">Developers</Link></li>
+          <li><Link to="!#">Developers</Link></li>
           <li><Link to="/register">Register</Link></li>
           <li><Link to="/login">Login</Link></li>
         </ul>
