b4c8bf4c62f959a9a8c3974bd90ed37db3ab791a
shazy89 <erdo.shazy123@gmail.com>
2021-01-25 13:46:29 -0500

    LOADED_USER action and populate the reducer with users info

--- client/src/utils/setAuthToken.js
+++ client/src/utils/setAuthToken.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
-import { delete } from 'request';
 
 const setAuthToken = token => {
     if(token) {
         axios.defaults.headers.common['x-auth-token'] = token;
     } else {
-        delete axios.defaults.headers.common['x-auth-token'];
+        delete axios.defaults.headers.common['x-auth-token']
     };
 };
-
 export default setAuthToken;
