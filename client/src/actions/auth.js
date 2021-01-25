4b142ec59063c2d922b25e7a0a8f98d59e582e59
shazy89 <erdo.shazy123@gmail.com>
2021-01-25 15:00:18 -0500

    token and auth

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import { setAlert } from './alert'
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL,
+         USER_LOADED, AUTH_ERROR,
+         LOGIN_SUCCESS, LOGIN_FAIL
+        } from './types';        
 import setAuthToken from '../utils/setAuthToken';
+
 //Lad User
 export const loadUser = () => async dispatch => {
     if(localStorage.token) {
@@ -33,8 +37,8 @@ export const register = ({ name, email, password }) => async dispatch => {
         dispatch({
             type: REGISTER_SUCCESS,
             payload: res.data
-        })
-
+        });
+        dispatch(loadUser());
     } catch (err) {
         const errors = err.response.data.errors;
         if(errors){
@@ -42,6 +46,31 @@ export const register = ({ name, email, password }) => async dispatch => {
         }
         dispatch({
             type: REGISTER_FAIL
-        })
-    }
-} 
+        });
+    };
+}; 
+//Login User
+export const login = ( email, password ) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+    const body = JSON.stringify({ email, password });
+    try {
+        const res = await axios.post('/api/auth', body, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+        dispatch(loadUser());
+    } catch (err) {
+        const errors = err.response.data.errors;
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+        dispatch({
+            type: LOGIN_FAIL
+        });
+    };
+}; 
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,9 @@
-import React, { Fragment, useState } from 'react'
-import { connect } from 'react-redux'
-import { Link } from 'react-router-dom';
-import { setAlert } from '../../actions/alert'
-import { register } from '../../actions/auth'
-import PropTypes from 'prop-types'
+import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
+import PropTypes from 'prop-types';
 
 const Register = ({ setAlert, register }) => {
     const [formData, setFormData] = useState({
@@ -37,6 +37,9 @@ const Register = ({ setAlert, register }) => {
          //  }
        }
    }
+   if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
 
     return (
         <Fragment>
@@ -92,7 +95,13 @@ const Register = ({ setAlert, register }) => {
         </Fragment>
     )
 };
-Register.prototypes = {
-    setAlert: PropTypes.func.isRequired
-}
-export default connect(null, { setAlert, register })(Register);
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
+const mapStateProps = {
+  isAuthenticated: state.auth.isAuthenticated
+};
+
+export default connect(mapStateProps, { setAlert, register })(Register);
