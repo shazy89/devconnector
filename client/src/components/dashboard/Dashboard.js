b74fadb7d796f6e13b31f987edb15142a8c68601
shazy89 <erdo.shazy123@gmail.com>
2021-01-26 11:40:01 -0500

    spinner

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,20 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, Fragment} from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner'
 import { getCurrentProfile } from '../../actions/profile';
 
-function Dashboard({getCurrentProfile, auth, profile}) {
+function Dashboard({getCurrentProfile, auth, profile: { profile, loading}}) {
     useEffect(() => {
         getCurrentProfile();
       }, [getCurrentProfile]);
-      
-    return (
-        <div>
-             Dashboard
-        </div>
-    )
-}
 
+return (
+    <div>
+        HELLO
+    </div>
+)
+}
 Dashboard.propTypes = {
     getCurrentProfile: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
