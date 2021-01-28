22c851f4f6883d3c892a19b818c93768e91f3e34
shazy89 <erdo.shazy123@gmail.com>
2021-01-28 13:55:21 -0500

    Profile Education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import { getProfileById } from "../../actions/profile";
 import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 
 const Profile = ({
   getProfileById,
@@ -51,6 +52,18 @@ const Profile = ({
               </Fragment>
             ) : (
               <h4>No experience credentials</h4>
+            )}
+          </div>
+          <div className="profile-edu bg-white p-2">
+            <h2 className="text-primary">Education</h2>
+            {profile.education.length > 0 ? (
+              <Fragment>
+                {profile.education.map((education) => (
+                  <ProfileEducation key={education._id} education={education} />
+                ))}
+              </Fragment>
+            ) : (
+              <h4>No education credentials</h4>
             )}
           </div>
         </Fragment>
