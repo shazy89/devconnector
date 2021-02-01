5cb99275433194322cf07d865fbc398925b21907
shazy89 <erdo.shazy123@gmail.com>
2021-02-01 09:39:11 -0500

    Post directory and post file

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,23 @@
+import React, { Fragment, useEffect } from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import Spinner from "../layout/Spinner";
+import PostItem from "../posts/PostItem";
+//import CommentForm from "../post/CommentForm";
+//import CommentItem from "../post/CommentItem";
+import { getPost } from "../../actions/post";
+
+const Post = (props) => {
+  return <div></div>;
+};
+
+Post.propTypes = {
+  getPost: PropTypes.func.isRequired,
+  post: PropTypes.object.isRequired,
+};
+const mapStateToProps = (state) => ({
+  post: state.post,
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
