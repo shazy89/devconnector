3e1f79d577e1a5168bc62a266714dc0e44133572
shazy89 <erdo.shazy123@gmail.com>
2021-02-01 11:13:15 -0500

    delete comment action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -141,3 +141,21 @@ export const addComment = (postId, formData) => async (dispatch) => {
     });
   }
 };
+// Delete comment
+export const deleteComment = (postId, commentId) => async (dispatch) => {
+  try {
+    await api.delete(`/posts/comment/${postId}/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
+
+    dispatch(setAlert("Comment Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
