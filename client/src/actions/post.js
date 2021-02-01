bc94667c4b6a1e60bb12c8df160e81e2605f6027
shazy89 <erdo.shazy123@gmail.com>
2021-02-01 08:25:44 -0500

    post action

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 import api from "../utils/api";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR, UPDATE_LIKES, DELETE_POST } from "./types";
+import {
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  DELETE_POST,
+  ADD_POST,
+} from "./types";
 
 // Get posts
 export const getPosts = () => async (dispatch) => {
@@ -65,6 +71,25 @@ export const deletePost = (id) => async (dispatch) => {
     });
 
     dispatch(setAlert("Post Removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add post
+export const addPost = (formData) => async (dispatch) => {
+  try {
+    const res = await api.post("/posts", formData);
+
+    dispatch({
+      type: ADD_POST,
+      payload: res.data,
+    });
+
+    dispatch(setAlert("Post Created", "success"));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
